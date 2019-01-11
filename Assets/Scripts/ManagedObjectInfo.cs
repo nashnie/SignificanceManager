a9ca9b6e9469affbf9ffcc0820efea811dce5984
聂述龙 <627972381@qq.com>
2019-01-11 23:03:08 +0800

    update base struct

---
+++ Assets/Scripts/ManagedObjectInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagedObjectInfo
+{
+    private UnityEngine.Object InObject;
+    private string InTag;
+    private float Significance = 1.0f;
+    private SignificanceManager.PostSignificanceType PostSignificanceType;
+
+    private SignificanceManager.FManagedObjectSignificanceFunction SignificanceFunction;
+    private SignificanceManager.FManagedObjectPostSignificanceFunction PostSignificanceFunction;
+
+    public UnityEngine.Object GetObject()
+    {
+        return InObject;
+    }
+
+    public string GetTag()
+    {
+        return InTag;
+    }
+
+    public float GetSignificance()
+    {
+        return Significance;
+    }
+
+    public SignificanceManager.FManagedObjectSignificanceFunction GetSignificanceFunction()
+    {
+        return SignificanceFunction;
+    }
+
+    public SignificanceManager.FManagedObjectPostSignificanceFunction GetPostSignificanceFunction()
+    {
+        return PostSignificanceFunction;
+    }
+
+    public SignificanceManager.PostSignificanceType GetPostSignificanceType()
+    {
+        return PostSignificanceType;
+    }
+
+    void UpdateSignificance(List<Transform> ViewPoints, bool bSortSignificanceAscending)
+    {
+
+    }
+}
--- Assets/Scripts/SignificanceManager.cs
+++ Assets/Scripts/SignificanceManager.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SignificanceManager : MonoBehaviour
 {
+    public delegate void FSignificanceFunction(UnityEngine.Object inObject, Transform transform);
+    public delegate void FPostSignificanceFunction(UnityEngine.Object inObject, float param1, float param2, bool param3);
+
+    public delegate void FManagedObjectSignificanceFunction(ManagedObjectInfo ManagedObjectInfo, Transform transform);
+    public delegate void FManagedObjectPostSignificanceFunction(ManagedObjectInfo ManagedObjectInfo, float param1, float param2, bool param3);
+
+    public enum PostSignificanceType
+    {
+        None,
+        Concurrent,
+        Sequntial,
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +30,53 @@ public class SignificanceManager : MonoBehaviour
     {
         
     }
+
+    public void RegisterObject(UnityEngine.Object InObject, string Tag, FSignificanceFunction SignificanceFunction, PostSignificanceType PostSignificanceType = PostSignificanceType.None, FPostSignificanceFunction PostSignificanceFunction = null)
+    {
+
+    }
+
+    public void RegisterObject(UnityEngine.Object InObject, string Tag, FManagedObjectSignificanceFunction ManagedObjectSignificanceFunction, PostSignificanceType PostSignificanceType = PostSignificanceType.None, FManagedObjectPostSignificanceFunction ManagedObjectPostSignificanceFunction = null)
+    {
+
+    }
+
+    public void UnregisterObject(UnityEngine.Object InObject)
+    {
+
+    }
+
+    public void UnregisterAll(string Tag)
+    {
+
+    }
+
+    public List<ManagedObjectInfo> GetManagedObjects(string Tag)
+    {
+        return null;
+    }
+
+    public void GetManagedObjects(List<ManagedObjectInfo> OutManagedObjects, bool bInSignificanceOrder = false)
+    {
+
+    }
+
+    public float GetSignificance(UnityEngine.Object InObject)
+    {
+        return 1.0f;
+    }
+
+    public bool QuerySignificance(UnityEngine.Object InObject, float OutSignificance)
+    {
+        return false;
+    }
+
+    public List<Transform> GetViewpoints()
+    {
+        return null;
+    }
+
+    //TODO proteced functuions and variables
 }
+
+
