ab0627c94cdb2a18e62c46d6ec3883398495b535
聂述龙 <627972381@qq.com>
2019-01-16 15:49:09 +0800

    add test class

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+    }
+}
